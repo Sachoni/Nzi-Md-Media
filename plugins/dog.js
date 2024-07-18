fe7041827c12ef8ab2ca1650e9606c9a2ab41c9e
Nzi <167908919+Sachoni@users.noreply.github.com>
2024-07-18 15:50:43 +0300

    dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -1,35 +1,35 @@
+
 /**
 
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //                                                                                                      //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
+//                    ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
 //                                                                                                      // 
-//                                         Ｖ：1．2．2                                                   // 
+//                                Ｖ：1．2．2                                                   // 
 //                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
-//                                                                                                      //
+//                       █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+                         █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+                         █░░║║║╠─║─║─║║║║║╠─░░█
+                         █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+                         █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+                                                                                           //
 //                                                                                                      //
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 
 CURRENTLY RUNNING ON BETA VERSION!!
 *
-   * @project_name : Suhail-Md
-   * @author : Suhail <https://github.com/SuhailTechInfo>
-   * @youtube : https://www.youtube.com/c/@SuhailTechInfo
-   * @infoription : Suhail-Md ,A Multi-functional whatsapp user bot.
-   * @version 1.2.5 
+   * @project_name : Nzi-Md
+   * @author : Nzi
+   * @youtube : https://www.youtube.com/c/@Sachoni
+   * @infoription : Nzi-Md ,A unique whatsapp bot.
+   * @version 1.2.2 
 *
    * Licensed under the  GPL-3.0 License;
 * 
-   * ┌┤Created By Suhail Tech Info.
-   * © 2023 Suhail-Md ✭ ⛥.
-   * plugin date : 10/12/2023
+   * ┌┤Created By Nzi
+   * © 2024 Nzi-Md ✭ ⛥.
+   * plugin date : 10/6/2024
 * 
    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -39,7 +39,6 @@ CURRENTLY RUNNING ON BETA VERSION!!
    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    * SOFTWARE.
 **/
-
 
 
 
@@ -71,7 +70,7 @@ const {
           let res = await fetch('https://random.dog/woof.json')
           let json = await res.json()
           if (json.status) return await m.reply("*Request Denied!*")
-          m.bot.sendFileUrl(m.jid, json.url,"", m, {author: "Suhail-Md" },"video");
+          m.bot.sendFileUrl(m.jid, json.url,"", m, {author: "Nzi-Md" },"video");
       
       }catch(e){ m.error(`${e}\n\nCommand: dog`,e,false)}
       })
